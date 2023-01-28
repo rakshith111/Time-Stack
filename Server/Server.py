28448d74d61037e2a9e31ebbff8139fa8ebf1a0f
rakshith111 <rakshithbabu111@gmail.com>
2023-01-28 20:12:28 +0530

    Update cli-Server Change api to receive

--- Server/Server.py
+++ Server/Server.py
@@ -6,21 +6,32 @@ import uvicorn
 class Server:
 
     def __init__(self):
+        '''
+        A simple server that can be used to POST (dump) and receive (message)
+        A dump function is provided to dump the data
+        A receive function is provided to receive data
+        '''
         self.router = APIRouter()
         self.router.add_api_route("/dump", self.dump, methods=["GET"])
-        self.router.add_api_route("/send", self.send, methods=["POST"])
+        self.router.add_api_route("/receive", self.receive, methods=["POST"])
         self.count = 0
         self.dumpstore = {"dump": {}}
 
 
-    def send(self, data: dict):
-        print("[+] [GET] /send")
+    def receive(self, data: dict):
+        '''
+        A function that receives data and stores it in a dictionary with a timestamp
+        '''
+        print("[+] [GET] /receive")
         convert = strftime("%H:%M:%S", gmtime())
         self.dumpstore["dump"][f'data{self.count}'] =data["message"]+ " @" f" {convert}"
         self.count += 1
         return "OK"
 
     def dump(self):
+        '''
+        A function that returns dumpstore data
+        '''
         print("[+] [POST] /dump")
         return self.dumpstore
 
