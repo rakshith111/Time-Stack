8b1b0e830deed922ae83aae1c9d9b0a0d7f87e18
MAX-dev2020 <monishvshetty7565@gmail.com>
2023-01-29 02:29:22 +0530

    coded mainactivity, added buttons

--- Android-app/TimeStackPrototype/app/src/main/java/com/example/timestackprototype/Dump.kt
+++ Android-app/TimeStackPrototype/app/src/main/java/com/example/timestackprototype/Dump.kt
@@ -1,6 +1,5 @@
 package com.example.timestackprototype
 
 data class Dump(
-    val server1 : String,
-    val server2 : String
+    val server1 : String
 )
--- Android-app/TimeStackPrototype/app/src/main/java/com/example/timestackprototype/MainActivity.kt
+++ Android-app/TimeStackPrototype/app/src/main/java/com/example/timestackprototype/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.timestackprototype
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.example.timestackprototype.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnGet.setOnClickListener {
+                try {
+                    TimeInterface.api.message()
+                } catch(e: IOException) {
+                    Log.e(TAG, "IOException, you might not have internet connection")
+                    txtView.text = getString(R.string.Error)
+                } catch (e: HttpException) {
+                    Log.e(TAG, "HttpException, unexpected response")
+                    txtView.text = getString(R.string.Error)
+                }
+            }
+        }
     }
 
 }
