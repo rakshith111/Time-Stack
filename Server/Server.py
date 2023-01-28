c0f1b379ce125a74aa9c21382968daf39b390107
rakshith111 <rakshithbabu111@gmail.com>
2023-01-28 19:27:21 +0530

    Update datadump content

--- Server/Server.py
+++ Server/Server.py
@@ -10,26 +10,25 @@ class Server:
         self.router.add_api_route("/dump", self.dump, methods=["GET"])
         self.router.add_api_route("/send", self.send, methods=["POST"])
         self.count = 0
-        self.map = {"dump": {}}
+        self.dumpstore = {"dump": {}}
 
-    def dump(self):
-        print(self.map)
-        return self.map
 
     def send(self, data: dict):
-        print(data)
-        # convert = strftime("%H:%M:%S", gmtime())
-        self.map["dump"][f'data{self.count}'] = data["message"]
+        print("[+] [GET] /send")
+        convert = strftime("%H:%M:%S", gmtime())
+        self.dumpstore["dump"][f'data{self.count}'] =data["message"]+ " @" f" {convert}"
         self.count += 1
         return "OK"
+
+    def dump(self):
+        print("[+] [POST] /dump")
+        return self.dumpstore
 
     def start(self):
         app = FastAPI()
         hello = Server()
         app.include_router(hello.router)
         uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
+        
 if __name__ == "__main__":
-    server = Server()
-    server.start()
+    Server().start()
