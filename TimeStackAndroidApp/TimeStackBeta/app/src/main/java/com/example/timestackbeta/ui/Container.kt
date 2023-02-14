7047f2123ddbfd9fd0a4d3395617b0f6f89aa265
MAX-dev2020 <monishvshetty7565@gmail.com>
2023-02-14 21:45:03 +0530

    stack design

---
+++ TimeStackAndroidApp/TimeStackBeta/app/src/main/java/com/example/timestackbeta/ui/Container.kt
@@ -0,0 +1,27 @@
+package com.example.timestackbeta.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Container(){
+   BoxWithConstraints(Modifier, Alignment.Center) {
+           LazyColumn(
+               modifier = Modifier.fillMaxWidth(),
+               verticalArrangement =  Arrangement.Top,
+           ) {
+                items(3){
+                    Loader()
+                }
+           }
+   }
+}
+
+
+
+
---
+++ TimeStackAndroidApp/TimeStackBeta/app/src/main/java/com/example/timestackbeta/ui/StackDesign.kt
@@ -0,0 +1,31 @@
+package com.example.timestackbeta.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
+import com.example.timestackbeta.R
+
+@Composable
+fun Loader(){
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    val videoLength = composition?.duration
+    println(videoLength)
+    if (videoLength != null) {
+        LottieAnimation(
+            composition,
+            modifier = Modifier.requiredHeight(100.dp),
+            iterations = LottieConstants.IterateForever,
+            clipSpec = LottieClipSpec.Progress(0f, 1f),
+            speed = videoLength/100000,
+            contentScale = ContentScale.FillHeight
+        )
+    }
+}
