7fab6f39a46dffe7ef41e044127e038bc92746f8
rakshith111 <rakshithbabu111@gmail.com>
2023-02-15 16:05:52 +0530

    Update Stack gen main to display Stack space

--- src/stack_gen_main.py
+++ src/stack_gen_main.py
@@ -5,29 +5,48 @@ import logging
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import QObject, QThread, QTimer
 import PyQt6.QtCore as QtCore
+from PyQt6.QtGui import QCloseEvent
 
-from ui_modules.Stackgen import Ui_Stackgen
+from ui_modules.Stack_gen import Ui_stack_gen
+from ui_modules.Stack_space import Ui_stack_space
 
 
-
-class Stackgenwindow(QtWidgets.QMainWindow):
+class StackSpace(QtWidgets.QWidget):
 
     def __init__(self, parent=None) -> None:
         '''
         Initializes the Stackgen class and sets up the UI.
         '''
 
-        super(Stackgenwindow, self).__init__(parent=parent)
+        super(StackSpace, self).__init__(parent=parent)
+        self.stack_gen_space_ui = Ui_stack_space()
+        self.stack_gen_space_ui.setupUi(self)
+        logger.info('StackSpace UI initialized')
 
-        self.stackgen_ui = Ui_Stackgen()
-        self.stackgen_ui.setupUi(self)
+    def closeEvent(self, event: QCloseEvent):
+        # Override the closeEvent and hide the window instead of closing it
+        event.ignore()
+        self.hide()
+
+class StackGen(QtWidgets.QWidget):
+
+    def __init__(self, parent=None) -> None:
+        '''
+        Initializes the Stackgen class and sets up the UI.
+        '''
+
+        super(StackGen, self).__init__(parent=parent)
+        self.stack_gen_ui = Ui_stack_gen()
+        self.stack_gen_ui.setupUi(self)
         logger.info('Stackgen UI initialized')
-        self.stackgen_ui.total_time_output.setText("00:00")
-        self.stackgen_ui.total_time_output.setAlignment(
+        self.stack_gen_ui.total_time_output.setText("00:00")
+        self.stack_gen_ui.total_time_output.setAlignment(
             QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.stackgen_ui.stack_name_input.setAlignment(
+        self.stack_gen_ui.stack_name_input.setAlignment(
             QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.stackgen_ui.create_stack_button.clicked.connect(self.create_stack)
+        self.stack_gen_ui.create_stack_button.clicked.connect(self.create_stack)
+        self.stack_gen_space = StackSpace()
+
 
     def create_stack(self) -> None:
         '''
@@ -38,9 +57,9 @@ class Stackgenwindow(QtWidgets.QMainWindow):
 
 
         '''
-        self.stack_name = self.stackgen_ui.stack_name_input.toPlainText()
-        self.start_time_input = self.stackgen_ui.start_time_input.time()
-        self.end_time_input = self.stackgen_ui.end_time_input.time()
+        self.stack_name = self.stack_gen_ui.stack_name_input.toPlainText()
+        self.start_time_input = self.stack_gen_ui.start_time_input.time()
+        self.end_time_input = self.stack_gen_ui.end_time_input.time()
         dt_time1 = datetime.time(
             self.start_time_input.hour(), self.start_time_input.minute())
         dt_time2 = datetime.time(
@@ -49,13 +68,13 @@ class Stackgenwindow(QtWidgets.QMainWindow):
                  datetime.datetime.combine(datetime.date.today(), dt_time1))
         temp = datetime.datetime.combine(
             datetime.date.today(), datetime.time()) + delta
-        self.stackgen_ui.total_time_output.setText(temp.strftime("%H:%M"))
-        self.stackgen_ui.total_time_output.setAlignment(
+        self.stack_gen_ui.total_time_output.setText(temp.strftime("%H:%M"))
+        self.stack_gen_ui.total_time_output.setAlignment(
             QtCore.Qt.AlignmentFlag.AlignCenter)
         # Create a new thread to run the stack generator
         logger.debug(
             f'Contents stack_name: {self.stack_name}, start_time_input: {dt_time1}, end_time_input: {dt_time2}, total_time_output: {temp}')
-
+        self.stack_gen_space.show()
 
 if __name__ == '__main__':
     logger = logging.getLogger('QTApp')
@@ -65,6 +84,6 @@ if __name__ == '__main__':
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     app = QtWidgets.QApplication(sys.argv)
-    w = Stackgenwindow()
+    w = StackGen()
     w.show()
     sys.exit(app.exec())
