81b452fe8395aa9638a0348eb14b9eb1fd3152e8
rakshith111 <rakshithbabu111@gmail.com>
2023-02-15 18:07:31 +0530

    Add add_stack method Add Stack Class Add functions to create lables

--- src/stack_gen_main.py
+++ src/stack_gen_main.py
@@ -1,10 +1,12 @@
 import datetime
-import sys
 import logging
+import logging.config
+import os
+import sys
 
+import PyQt6.QtCore as QtCore
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import QObject, QThread, QTimer
-import PyQt6.QtCore as QtCore
 from PyQt6.QtGui import QCloseEvent
 
 from ui_modules.Stack_gen import Ui_stack_gen
@@ -16,17 +18,41 @@ class StackSpace(QtWidgets.QWidget):
     def __init__(self, parent=None) -> None:
         '''
         Initializes the Stackgen class and sets up the UI.
+
         '''
 
         super(StackSpace, self).__init__(parent=parent)
         self.stack_gen_space_ui = Ui_stack_space()
         self.stack_gen_space_ui.setupUi(self)
-        logger.info('StackSpace UI initialized')
+        self.custom_widget = Stack()
+        scroll_layout = QtWidgets.QVBoxLayout()
+        self.vertical_scrollbar = self.stack_gen_space_ui.stack_area_scrollable.verticalScrollBar()
+        self.stack_gen_space_ui.stack_area_scrollable.widget().setLayout(scroll_layout)
+
+    def add_stack(self, name, start_time, end_time) -> None:
+        '''
+        Adds a custom widget to the scroll area.
+
+        Currently creates a new label and adds it to the scroll area.
+        This function is called when the create stack button is clicked.
+        For now it just creates a label with the name of the stack.
+
+        '''
+
+        label = self.custom_widget.create_label(name=name)
+        scroll_layout = self.stack_gen_space_ui.stack_area_scrollable.widget().layout()
+        scroll_layout.addWidget(label)
+        logger.info(f'Label created with name {name}')
+        logger.debug(
+            f'Parms name: {name}, start_time: {start_time}, end_time: {end_time}')
+        logger.debug(
+            f'Scrollbar vsibility is  {self.vertical_scrollbar.isVisible()}')
 
     def closeEvent(self, event: QCloseEvent):
         # Override the closeEvent and hide the window instead of closing it
         event.ignore()
         self.hide()
+
 
 class StackGen(QtWidgets.QWidget):
 
@@ -44,9 +70,9 @@ class StackGen(QtWidgets.QWidget):
             QtCore.Qt.AlignmentFlag.AlignCenter)
         self.stack_gen_ui.stack_name_input.setAlignment(
             QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.stack_gen_ui.create_stack_button.clicked.connect(self.create_stack)
-        self.stack_gen_space = StackSpace()
-
+        self.stack_gen_ui.create_stack_button.clicked.connect(
+            self.create_stack)
+        self.stack_space = StackSpace()
 
     def create_stack(self) -> None:
         '''
@@ -54,6 +80,7 @@ class StackGen(QtWidgets.QWidget):
         This function is called when the create stack button is clicked.
         Converts the start and end time to datetime.time objects and then calculates the total time.
         has to be done this way because the time objects are not compatible with the datetime module.
+        Calls the `add_stack` function in the `StackSpace` class to add the stack to the scroll area.
 
 
         '''
@@ -74,15 +101,37 @@ class StackGen(QtWidgets.QWidget):
         # Create a new thread to run the stack generator
         logger.debug(
             f'Contents stack_name: {self.stack_name}, start_time_input: {dt_time1}, end_time_input: {dt_time2}, total_time_output: {temp}')
-        self.stack_gen_space.show()
+        self.stack_space.add_stack(self.stack_name, dt_time1, dt_time2)
+        self.stack_space.show()
+
+
+class Stack(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        '''
+        Initializes the Stack class and sets up the UI.
+
+        '''
+        super().__init__(parent)
+
+    def create_label(self, name) -> QtWidgets.QLabel:
+        '''
+        :param str name: The name of the stack
+        :return: A label with the name of the stack
+        Creates a label with the name of the stack.
+        For now it just creates a label with the name of the stack.
+        later it will create a custom widget with the name of the stack and the start and end time.
+        '''
+
+        label = QtWidgets.QLabel(name)
+        label.setFixedSize(200, 100)  # Set the label's fixed size
+        return label
+
 
 if __name__ == '__main__':
+
+    logger_config_path = r'src\logging.ini'
+    logging.config.fileConfig(os.path.abspath(logger_config_path))
     logger = logging.getLogger('QTApp')
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('QTApp.log')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%I:%M:%S %p %m/%d/%Y')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
     app = QtWidgets.QApplication(sys.argv)
     w = StackGen()
     w.show()
