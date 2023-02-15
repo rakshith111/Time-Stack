b86cb406ee1c9357e5ce1d0db0e95ddedf0b30ff
MAX-dev2020 <monishvshetty7565@gmail.com>
2023-02-15 13:45:53 +0530

    container

--- TimeStackAndroidApp/TimeStackBeta/app/src/main/java/com/example/timestackbeta/ui/Container.kt
+++ TimeStackAndroidApp/TimeStackBeta/app/src/main/java/com/example/timestackbeta/ui/Container.kt
@@ -1,6 +1,5 @@
 package com.example.timestackbeta.ui
 
-import android.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,19 +10,28 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Container(){
-
-            LazyColumn(verticalArrangement = Arrangement.Center){
-                items(3){item->
+    Box(
+        modifier = Modifier
+            .background(color = androidx.compose.ui.graphics.Color.Blue)
+            .fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Box(contentAlignment = Alignment.Center,modifier = Modifier
+            .size(200.dp, 500.dp)
+            .clip(shape = RoundedCornerShape(size = 12.dp))
+            .background(color = Color(0xFF82D8FF))) {
+            LazyColumn(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+                items(19) { item ->
                     Loader()
                 }
             }
-
-
-
+        }}
 
 }
 
