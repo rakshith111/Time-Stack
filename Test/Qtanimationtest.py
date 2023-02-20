07590894a7016d631780daec921d26f9ea28e6be
rakshith111 <rakshithbabu111@gmail.com>
2023-02-20 21:52:45 +0530

    Add URL input for faster testing

--- Test/Qtanimationtest.py
+++ Test/Qtanimationtest.py
@@ -1,4 +1,5 @@
 
+from PyQt6 import QtCore, QtWidgets, QtQuickWidgets
 import os
 import subprocess
 import sys
@@ -8,68 +9,62 @@ from PyQt6 import QtCore, QtQuickWidgets, QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QLabel, QScrollArea, QVBoxLayout,
                              QWidget)
-
-CURRENT_DIRECTORY = Path(__file__).resolve().parent
-if __name__ == "__main__":
-
-
-    app = QtWidgets.QApplication(sys.argv)
-    label_widget = QtWidgets.QLabel("Animation", alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-    widget = QtWidgets.QWidget()
-    box_layout = QtWidgets.QVBoxLayout()
-    subprocess.Popen([sys.executable, "-m", "http.server", "-b", "127.0.0.1", "-d","test/animation"])
-    widget.setLayout(box_layout)
-    animation_widget = QtQuickWidgets.QQuickWidget(resizeMode=QtQuickWidgets.QQuickWidget.ResizeMode.SizeRootObjectToView )
-    filename = os.fspath(CURRENT_DIRECTORY / "player.qml")
-    url = QtCore.QUrl.fromLocalFile(filename)
-    animation_widget.setSource(url)
-    box_layout.addWidget(label_widget, stretch=0, )
-    box_layout.addWidget(animation_widget, stretch=2)
-    widget.resize(600, 600)
-    widget.show()
-
-    sys.exit(app.exec())
+from PyQt6.QtCore import QObject, QVariant
+from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtQml import QQmlApplicationEngine
 
 
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # Create the input field
+        self.input_field = QtWidgets.QLineEdit(self)
+        self.input_field.setPlaceholderText("Enter a JSON file URL")
+        self.input_field.setText("http://127.0.0.1:8000/full_loading.json")
 
+        # Create the submit button
+        self.submit_button = QtWidgets.QPushButton("Submit", self)
+        self.submit_button.clicked.connect(self.submit)
+        label_widget = QtWidgets.QLabel(
+            "Animation", alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        box_layout = QtWidgets.QVBoxLayout(self)
+        box_layout.addWidget(label_widget, stretch=0)
+        box_layout.addWidget(self.input_field)
+        box_layout.addWidget(self.submit_button)
 
-# class ScrollableWindow(QWidget):
-#     def __init__(self):
-#         super().__init__()
-
-#         # create a scroll area widget and set its properties
-#         scroll_area = QScrollArea()
-#         scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-#         scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-#         scroll_area.setWidgetResizable(True)
-
-#         # create a widget to hold the content of the scroll area
-#         content_widget = QWidget(scroll_area)
-#         scroll_area.setWidget(content_widget)
-
-#         # create a layout for the content widget
-#         content_layout = QVBoxLayout(content_widget)
-#         content_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-
-#         # add some content to the content layout
-#         animation_widget = QtQuickWidgets.QQuickWidget(
-#             resizeMode=QtQuickWidgets.QQuickWidget.ResizeMode.SizeRootObjectToView
-#         )
-#         filename = os.fspath(CURRENT_DIRECTORY / "test.qml")
-#         url = QtCore.QUrl.fromLocalFile(filename)
-#         animation_widget.setSource(url)
-#         content_layout.addWidget(animation_widget)
+        # Start a local HTTP server to serve the QML file
+        subprocess.Popen([sys.executable, "-m", "http.server",
+                         "-b", "127.0.0.1", "-d", "test/animation"])
         
 
-#         # create a layout for the scrollable window
-#         main_layout = QVBoxLayout(self)
-#         main_layout.addWidget(scroll_area)
+        # Create the QML widget
+        self.animation_widget = QtQuickWidgets.QQuickWidget(
+            self, resizeMode=QtQuickWidgets.QQuickWidget.ResizeMode.SizeRootObjectToView)
+        filename = os.fspath(CURRENT_DIRECTORY / "player.qml")
+        url = QtCore.QUrl.fromLocalFile(filename)
+        self.animation_widget.setSource(url)
+        box_layout.addWidget(self.animation_widget, stretch=2)
+
+        self.resize(600, 600)
+        self.show()
+
+        # Set a property on the root QML object to pass data from Python to QML
+        self.root_object = self.animation_widget.rootObject()
+ 
+
+    def submit(self):
+        # Get the value of the input field
+        input_value = self.input_field.text()
+
+        # Use the value to update the QML animation
+
+        self.root_object.setProperty("myProperty", input_value)
 
 
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
 
-#     window = ScrollableWindow()
-#     window.show()
-
-#     sys.exit(app.exec())
+if __name__ == "__main__":
+    
+    CURRENT_DIRECTORY = Path(__file__).resolve().parent
+    app = QtWidgets.QApplication(sys.argv)
+    widget = MyWidget()
+    sys.exit(app.exec())
--- Test/player.qml
+++ Test/player.qml
@@ -1,34 +1,37 @@
-
 import QtQuick 2.15
 import Qt.labs.lottieqt 6.3
-
 
 Item {
     width: 600
     height: 600
-    LottieAnimation {
-        anchors.fill: parent
-        loops: LottieAnimation.Infinite
-        direction: LottieAnimation.Forward
-        // working small_play.json,loading.json,smal.json
-        // play.json long time to load 
-        // rest of the json files not working
-        source: "http://127.0.0.1:8000/full_loading.json"
-        onStatusChanged : {
-            if (status === LottieAnimation.Ready)
-            {
-                frameRate = 60;
-                gotoAndPlay(startFrame);
-                console.log("Ready")
-                console.log("startFrame: " + startFrame)
-                console.log("endFrame: " + endFrame)
-                console.log("frameRate: " + frameRate)
-                console.log("direction: " + direction)
-                console.log("loops: " + loops)
 
+    property string myProperty: ""
+        LottieAnimation {
+            anchors.fill: parent
+            loops: LottieAnimation.Infinite
+            direction: LottieAnimation.Forward
+
+            // working full_loading.json, small_play.json, loading.json, smal.json, stars.json, online_converted.json
+            // play.json long time to load
+            // rest of the json files not working
+
+            source: myProperty
+
+            onStatusChanged: {
+                console.log("status", status);
+                if (status === LottieAnimation.Ready)
+                {
+                    frameRate = 60;
+                    gotoAndPlay(startFrame+1);
+                    console.log('source', source)
+                    console.log("Ready")
+                    console.log("startFrame: " + startFrame)
+                    console.log("endFrame: " + endFrame)
+                    console.log("frameRate: " + frameRate)
+                    console.log("direction: " + direction)
+                    console.log("loops: " + loops)
+
+                }
             }
         }
-
     }
-}
-
