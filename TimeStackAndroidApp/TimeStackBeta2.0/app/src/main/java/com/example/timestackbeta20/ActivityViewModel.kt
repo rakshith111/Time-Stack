18fbf5621a5955366aaf00b13a30a9af0ce3ca46
rakshith111 <rakshithbabu111@gmail.com>
2023-02-27 16:32:50 +0530

    Remove app

--- TimeStackAndroidApp/TimeStackBeta2.0/app/src/main/java/com/example/timestackbeta20/ActivityViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.timestackbeta20
-
-import androidx.lifecycle.ViewModel
-import com.example.timestackbeta20.data.ActivityData
-
-class ActivityViewModel : ViewModel(){
-    val stacks = mutableListOf<ActivityData>()
-    2
-}
