85cd40b4f61ef68a54cbaf2dac7cd7b37422accc
MAX-dev2020 <monishvshetty7565@gmail.com>
2023-02-27 19:58:41 +0530

    new file

--- TimeStackAndroidApp/TimeStackArch/app/src/main/java/com/example/TimeStackArch/data/StackData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.timestackarch.data
-
-data class StackData(
-    val id : Int,
-    val stackName : String,
-    val stackTime : String,
-    val activeStack : String,
-    val stackProgress : Int
-)
