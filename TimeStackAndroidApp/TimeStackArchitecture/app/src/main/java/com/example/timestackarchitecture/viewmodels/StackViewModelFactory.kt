de23e86e0b595847f0ca0a1ba373fb9a538b835b
MAX-dev2020 <60339025+MAX-dev2020@users.noreply.github.com>
2023-03-09 20:41:39 +0530

    added database, implemented dagger HILT

---
+++ TimeStackAndroidApp/TimeStackArchitecture/app/src/main/java/com/example/timestackarchitecture/viewmodels/StackViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.timestackarchitecture.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.timestackarchitecture.data.StackRepository
+import javax.inject.Inject
+
+
+class StackViewModelFactory @Inject constructor(private val repository: StackRepository):
+    ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>):
+            T = StackViewModel(repository) as T
+}
+
+class TimerViewModelFactory :
+    ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>):
+            T = TimerViewModel() as T
+}
