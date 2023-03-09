bf074bc2861f05ca674e5c3566c907e17c12bf4c
MAX-dev2020 <60339025+MAX-dev2020@users.noreply.github.com>
2023-03-09 21:55:23 +0530

    few changes

--- TimeStackAndroidApp/TimeStackArchitecture/app/src/main/java/com/example/timestackarchitecture/compose/BaseScreen.kt
+++ TimeStackAndroidApp/TimeStackArchitecture/app/src/main/java/com/example/timestackarchitecture/compose/BaseScreen.kt
@@ -14,19 +14,17 @@ fun BaseScreen(
 ) {
     val list = stackViewModel.stackList.collectAsState(initial = emptyList())
     val stackList = list.value
-    val startTimerRef = { totalPlayedTime: Int ->
-        timerViewModel.startTimer(totalPlayedTime)
-    }
-    val stopTimerRef = { pauseTimer: (Int) -> Unit ->
-        timerViewModel.stopTimer(pauseTimer)
-    }
 
     val selectedItems = stackViewModel.selectedItems
 
     Container(
         stackList, selectedItems,
-        startTimerRef,
-        stopTimerRef , insertStack = { stackData: StackData ->
+        startTimer = { totalPlayedTime: Int ->
+            timerViewModel.startTimer(totalPlayedTime)
+        },
+        stopTimer = { pauseTimer: (Int) -> Unit ->
+            timerViewModel.stopTimer(pauseTimer)
+        } , insertStack = { stackData: StackData ->
             stackViewModel.insertStack(stackData) },
         removeStack = { stackData: StackData -> stackViewModel.removeStack(stackData) }
     ) { stackViewModel.clearAll() }
