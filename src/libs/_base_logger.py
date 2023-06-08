04169ad4a92851676617fd8ef11313be68612689
rakshith111 <rakshithbabu111@gmail.com>
2023-06-08 14:45:50 +0530

    Update to google doc format

--- src/libs/_base_logger.py
+++ src/libs/_base_logger.py
@@ -6,4 +6,5 @@ log_file_path = path.join(path.dirname(
         path.dirname(path.abspath(__file__))),'configs', 'logging.ini')
 logging.config.fileConfig(log_file_path)
 logger=logging.getLogger('QTApp')
-CURRENT_DIR = path.dirname(path.abspath(__file__))
+BASE_DIR = path.join(path.dirname(
+        path.dirname(path.abspath(__file__))))
