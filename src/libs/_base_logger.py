d930f2f739bc29363bab0060d8d14356709bf453
rakshith111 <rakshithbabu111@gmail.com>
2023-06-08 13:58:04 +0530

    Rename paths

--- src/libs/_base_logger.py
+++ src/libs/_base_logger.py
@@ -1,8 +1,9 @@
 import logging
 import logging.config
 from os import path
+# Go up one level to the src folder
 log_file_path = path.join(path.dirname(
-        path.abspath(__file__)), 'logging.ini')
+        path.dirname(path.abspath(__file__))),'configs', 'logging.ini')
 logging.config.fileConfig(log_file_path)
 logger=logging.getLogger('QTApp')
 CURRENT_DIR = path.dirname(path.abspath(__file__))
