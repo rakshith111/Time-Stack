a225d17f028cbee91895bb75a31f51ddf7d6eb22
Monish V Shetty <monishvshetty7565@gmail.com>
2023-06-20 14:25:48 +0530

    Network layer

---
+++ TimeStackAndroidApp/NetworkPrototype/app/src/main/java/com/example/networkprototypeapp/network/TimeStackInterface.kt
@@ -0,0 +1,16 @@
+package com.example.networkprototypeapp.network
+
+import com.example.networkprototypeapp.data.TimeStackData
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface TimeStackInterface {
+    @GET("/dump")
+    fun getMessage(): Call<Map<String, Any>>
+
+    @POST("/receive")
+    fun postMessage(@Body messageData: TimeStackData):Call<TimeStackData>
+
+}
---
+++ TimeStackAndroidApp/NetworkPrototype/app/src/main/java/com/example/networkprototypeapp/viewmodel/ApiViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.networkprototypeapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.networkprototypeapp.data.TimeStackData
+import com.example.networkprototypeapp.network.GetService
+import kotlinx.coroutines.flow.MutableStateFlow
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ApiViewModel : ViewModel(){
+    private var _dashboardData = MutableStateFlow<Map<String, Any>>(mapOf())
+
+    val dashboardData: MutableStateFlow<Map<String, Any>> = _dashboardData
+
+    fun makeGetApiRequest() {
+        GetService().getApiInterface("").getMessage()
+            .enqueue(object : Callback<Map<String, Any>> {
+                override fun onResponse(
+                    call: Call<Map<String, Any>>,
+                    response: Response<Map<String, Any>>
+                ) {
+                    println("Response: ${response.body()}")
+                    println("Response: ${response.raw()}")
+                    // Process the response data here
+                    _dashboardData.value = response.body() ?: mapOf()
+                }
+
+                override fun onFailure(call: Call<Map<String, Any>>, t: Throwable) {
+                    println("Error: ${t.message}")
+                    // Handle the error here
+                }
+            })
+    }
+}
