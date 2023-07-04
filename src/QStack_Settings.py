2313ffeec9700247b76137cbd4d1320f2c7fc153
rakshith111 <rakshithbabu111@gmail.com>
2023-07-04 22:36:05 +0530

    Update settings ui to include sounds Add sounds

--- src/QStack_Settings.py
+++ src/QStack_Settings.py
@@ -1,8 +1,11 @@
 import json
-from os import path
+from os import path,listdir
 
+from PyQt6.QtCore import QUrl
 from PyQt6 import QtGui, QtWidgets
 from PyQt6.QtGui import QCloseEvent
+from PyQt6.QtMultimedia import QSoundEffect
+
 
 from libs.color import Color
 from libs._base_logger import logger
@@ -14,7 +17,11 @@ from libs.QClasses.QToggle import AnimatedToggle
 DEFAULT_SETTINGS = {
     "theme": "dark",
     "close_to_tray": True,
-    "notification": True
+    "notification": True,
+    "notification_sound_general": "upset(Default_gen).wav",
+    "notification_sound_midway": "that-was-quick(Default_mid).wav",
+    "notification_sound_quarterly": "good-news(Default_quat).wav",
+    "notification_sound_end": "strong-minded(Default_end).wav",
 }
 
 SETTINGS=path.join(BASE_DIR,"user_data" ,"settings.json")
@@ -43,7 +50,6 @@ class SettingsManager:
             self.save_settings()
 
 
-
 class SettingsWindow(QtWidgets.QWidget):
     def __init__(self, parent,manipulate_window ) -> None:
         super().__init__(parent=parent)
@@ -62,14 +68,11 @@ class SettingsWindow(QtWidgets.QWidget):
         self.toggle_theme_btn.setFixedSize(self.toggle_theme_btn.sizeHint())
         self.toggle_theme_btn.setObjectName("toggle_theme_btn")
         
-        
         self.settings_ui.main_app_layout.replaceWidget(self.settings_ui.theme_toggle_placeholder,self.toggle_theme_btn)
         self.settings_ui.theme_toggle_placeholder.deleteLater()
         self.toggle_theme_btn.setChecked(self.local_settings.settings["theme"]=="dark")
         self.toggle_theme_btn.stateChanged.connect(self.update_theme)
         self.manipulate_window.current_theme = self.local_settings.settings["theme"]
-        
-
 
         # Close to tray btn
         self.toggle_close_to_tray_btn=AnimatedToggle()
@@ -91,12 +94,73 @@ class SettingsWindow(QtWidgets.QWidget):
         self.settings_ui.main_app_layout.replaceWidget(self.settings_ui.notification_placeholder,self.toggle_notification_btn)
         self.settings_ui.notification_placeholder.deleteLater()
         self.toggle_notification_btn.setChecked(self.local_settings.settings["notification"])
-        self.manipulate_window.notifications_enabled = self.local_settings.settings["notification"]
+        self.manipulate_window.stack_space.manager.notifications_enabled = self.local_settings.settings["notification"]
         # change bool value of notification on state change
         self.toggle_notification_btn.stateChanged.connect(self.toggle_notification_update)
-       
+        self.populate_notification_sounds()
         
+    def populate_notification_sounds(self):
+        self.notification_sounds = []
+        for sound in listdir(path.join(BASE_DIR, "ui_files", "sounds")):
+            self.notification_sounds.append(sound)
+        self.settings_ui.general_notification_sound.addItems(self.notification_sounds)
+        self.settings_ui.midway_notification_sound.addItems(self.notification_sounds)
+        self.settings_ui.quarterly_notification_sound.addItems(self.notification_sounds)
+        self.settings_ui.end_notification_sound.addItems(self.notification_sounds)
+        self.settings_ui.general_notification_sound.setCurrentText(self.local_settings.settings["notification_sound_general"])
+        self.settings_ui.midway_notification_sound.setCurrentText(self.local_settings.settings["notification_sound_midway"])
+        self.settings_ui.quarterly_notification_sound.setCurrentText(self.local_settings.settings["notification_sound_quarterly"])
+        self.settings_ui.end_notification_sound.setCurrentText(self.local_settings.settings["notification_sound_end"])
+        self.settings_ui.general_notification_sound.currentTextChanged.connect(self.update_general_notification_sound)
+        self.settings_ui.midway_notification_sound.currentTextChanged.connect(self.update_midway_notification_sound)
+        self.settings_ui.quarterly_notification_sound.currentTextChanged.connect(self.update_quarterly_notification_sound)
+        self.settings_ui.end_notification_sound.currentTextChanged.connect(self.update_end_notification_sound)
+        self.manipulate_window.stack_space.manager.notification_sound_general = self.local_settings.settings["notification_sound_general"]
+        self.manipulate_window.stack_space.manager.notification_sound_midway = self.local_settings.settings["notification_sound_midway"]
+        self.manipulate_window.stack_space.manager.notification_sound_quarterly = self.local_settings.settings["notification_sound_quarterly"]
+        self.manipulate_window.stack_space.manager.notification_sound_end = self.local_settings.settings["notification_sound_end"]
+        logger.info(f'{Color.CVIOLET}Notification sounds set{Color.ENDC}')
 
+    def play_sound(self,sound:str)->None:
+
+        self.sound = QSoundEffect()
+        self.sound.setSource(QUrl.fromLocalFile(path.join(BASE_DIR, "ui_files", "sounds", sound)))
+        self.sound.setVolume(1)
+        self.sound.play()
+
+    def update_general_notification_sound(self):
+
+        self.local_settings.settings["notification_sound_general"] = self.settings_ui.general_notification_sound.currentText()
+        self.manipulate_window.stack_space.manager.notification_sound_general = self.local_settings.settings["notification_sound_general"]
+        logger.info(f'{Color.CVIOLET}General notification sound set to {self.local_settings.settings["notification_sound_general"]}{Color.ENDC}')
+        self.local_settings.save_settings()
+        self.play_sound(self.local_settings.settings["notification_sound_general"])         
+        
+    
+    def update_midway_notification_sound(self):
+            
+        self.local_settings.settings["notification_sound_midway"] = self.settings_ui.midway_notification_sound.currentText()
+        self.manipulate_window.stack_space.manager.notification_sound_midway = self.local_settings.settings["notification_sound_midway"]
+        logger.info(f'{Color.CVIOLET}Midway notification sound set to {self.local_settings.settings["notification_sound_midway"]}{Color.ENDC}')
+        self.local_settings.save_settings()
+        self.play_sound(self.local_settings.settings["notification_sound_midway"])
+
+    def update_quarterly_notification_sound(self):
+                
+        self.local_settings.settings["notification_sound_quarterly"] = self.settings_ui.quarterly_notification_sound.currentText()
+        self.manipulate_window.stack_space.manager.notification_sound_quarterly = self.local_settings.settings["notification_sound_quarterly"]
+        logger.info(f'{Color.CVIOLET}Quarterly notification sound set to {self.local_settings.settings["notification_sound_quarterly"]}{Color.ENDC}')
+        self.local_settings.save_settings()
+        self.play_sound(self.local_settings.settings["notification_sound_quarterly"])
+
+    def update_end_notification_sound(self):
+                    
+        self.local_settings.settings["notification_sound_end"] = self.settings_ui.end_notification_sound.currentText()
+        self.manipulate_window.stack_space.manager.notification_sound_end = self.local_settings.settings["notification_sound_end"]
+        logger.info(f'{Color.CVIOLET}End notification sound set to {self.local_settings.settings["notification_sound_end"]}{Color.ENDC}')
+        self.local_settings.save_settings()
+        self.play_sound(self.local_settings.settings["notification_sound_end"])
+        
     def toggle_close_to_tray_update(self):
        
         self.local_settings.settings["close_to_tray"] = self.toggle_close_to_tray_btn.isChecked()
@@ -120,6 +184,9 @@ class SettingsWindow(QtWidgets.QWidget):
         self.manipulate_window.stack_space.manager.notifications_enabled = self.local_settings.settings["notification"]
         self.local_settings.save_settings()
         self.manipulate_window.toggle_theme()
+        if self.local_settings.settings["notification"]:
+            # Hide the notification if it is visible
+            pass
         
     def closeEvent(self, a0: QCloseEvent) -> None:
         self.hide()
