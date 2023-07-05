c3ccc23892978bc6d2758f099ddad8ef7a4bf495
rakshith111 <rakshithbabu111@gmail.com>
2023-07-05 17:28:21 +0530

    set ip

--- servers/TornadoSocket/tornado_main_server.py
+++ servers/TornadoSocket/tornado_main_server.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import socket
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
@@ -19,10 +20,24 @@ def make_app():
     return tornado.web.Application([
         (r"/socket", SocketHandler),
     ])
+def get_local_ip()->None:
+    '''
 
+    A function that returns the local ip address.
+    It makes a connection to google's dns server and returns the local ip address.    
+
+    '''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except:
+        return None
 if __name__ == "__main__":
     app = make_app()
     # Set ip
-    app.listen(8888,address='127.0.0.1')
+    app.listen(8888,address=get_local_ip())
     print("Server started on port 8888")
     tornado.ioloop.IOLoop.current().start()
