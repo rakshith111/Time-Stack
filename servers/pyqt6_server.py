6a8e126296b3ff9503b5804f30a74ae5b50304fd
rakshith111 <rakshithbabu111@gmail.com>
2023-07-29 13:30:09 +0530

    Add proper endpoints for receiving data

--- servers/pyqt6_server.py
+++ servers/pyqt6_server.py
@@ -7,11 +7,9 @@ import random
 import string
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton, QLabel, QVBoxLayout, QWidget
 from PyQt6.QtGui import QPixmap
-
-
 from PyQt6.QtWebSockets import QWebSocketServer
 from PyQt6.QtNetwork import QHostAddress
-from pyqrcode import QRCode 
+from pyqrcode import QRCode
 
 
 def get_local_ip() -> str:
@@ -27,7 +25,8 @@ def get_local_ip() -> str:
         return local_ip
     except:
         return None
-    
+
+
 class WebSocketServer(QWebSocketServer):
     def __init__(self, address, port, text_edit):
         super().__init__("WebSocket Server", QWebSocketServer.SslMode.NonSecureMode)
@@ -35,29 +34,34 @@ class WebSocketServer(QWebSocketServer):
         self.text_edit = text_edit
         self.any_client_connected = []
         self.authed_clients = {}
-        self.challenge_code=None
+        self.challenge_code = None
+        self.endpoints = {
+            "pause_top": self.on_pause,
+            "start_top": self.on_play,
+            "pop_top": self.on_pop,
+            "new_activity": self.on_new_activity,
+        }
         self.newConnection.connect(self.on_new_connection)
         if not os.path.exists("challenge_code.json"):
             with open("challenge_code.json", "w") as f:
                 json.dump({}, f)
         self.load_authed_clients()
+
     def load_authed_clients(self):
-        
         with open("challenge_code.json", "r") as f:
             self.authed_clients = json.load(f)
 
     def save_authed_clients(self):
         with open("challenge_code.json", "w") as f:
             json.dump(self.authed_clients, f)
-            
-    def update_challenge_code(self,codes):
+
+    def update_challenge_code(self, codes):
         self.challenge_code = codes[0]
         print(f"Challenge code: {self.challenge_code}")
-    
+
     def on_new_connection(self):
         sender = self.sender()
         sender_ip = sender.peerAddress().toString()
-        # previously authed client
 
         if sender_ip in self.authed_clients.values():
             print("Processing previously authed client")
@@ -65,26 +69,23 @@ class WebSocketServer(QWebSocketServer):
             self.socket.textMessageReceived.connect(self.process_auth_text_message)
             self.socket.disconnected.connect(self.client_disconnected)
             self.any_client_connected.append(self.socket)
-        # new client
         elif self.challenge_code:
             print("Processing new client")
             self.socket = self.nextPendingConnection()
             self.socket.textMessageReceived.connect(self.process_text_message)
             self.socket.disconnected.connect(self.client_disconnected)
-            self.any_client_connected.append(self.socket)        
+            self.any_client_connected.append(self.socket)
             for i in self.any_client_connected:
                 print(i.peerAddress().toString())
             self.save_authed_clients()
         else:
             print("Disconnecting client as no challenge code is set")
-            # Disconnect the client after sending error message
+            # Disconnect the client after sending an error message
             self.socket = self.nextPendingConnection()
-            self.socket.sendTextMessage(json.dumps({"challenge_code_accepted":False,"error":"No challenge code is set"}))
+            self.socket.sendTextMessage(json.dumps({"challenge_code_accepted": False, "error": "No challenge code is set"}))
             self.socket.disconnect()
             self.socket.deleteLater()
-
             self.socket = None
-
 
     def process_auth_text_message(self, message):
         sender = self.sender()
@@ -95,43 +96,33 @@ class WebSocketServer(QWebSocketServer):
             # Reverse lookup key from value
             if self.check_key(json_data):
                 key = list(self.authed_clients.keys())[list(self.authed_clients.values()).index(sender_ip)]
-                if key== json_data["challenge_code"]:
+                if key == json_data["challenge_code"]:
                     self.text_edit.append("Challenge code matched")
                     self.text_edit.append("welcome old user")
                     self.text_edit.append("Client connected")
-                    self.socket.sendTextMessage(json.dumps({"Verified":True}))
+                    self.socket.sendTextMessage(json.dumps({"Verified": True}))
                 try:
                     if json_data["endpoint"]:
-                        endpoint=True
-                except  Exception as e:
-                    endpoint=False
+                        endpoint = True
+                except Exception as e:
+                    endpoint = False
                 if endpoint:
                     self.text_edit.append("endpoint received")
                     self.text_edit.append("Client connected")
                     self.text_edit.append(f"Client address: {sender_ip}")
                     self.text_edit.append(f"endpoint: {json_data['endpoint']}")
-                    print(json_data)
-                    print(type(json_data))
-                    if json_data['endpoint'] == "pause":
-                        self.text_edit.append("pause")
-                    elif json_data['endpoint'] == "play":
-                        self.text_edit.append("play")
-                    elif json_data['endpoint'] == "next":
-                        self.text_edit.append("next")
-                    elif json_data['endpoint'] == "previous":
-                        self.text_edit.append("previous")
-                    
-                    
-                    
+                    if json_data['endpoint'] in self.endpoints:
+                        self.endpoints[json_data['endpoint']]()
+
             else:
                 self.text_edit.append("Challenge code did not match")
                 self.text_edit.append("Client not authenticated")
                 self.text_edit.append("Client disconnected")
-                self.socket.sendTextMessage(json.dumps({"challenge_code_accepted":False,"error":"Challenge code did not match for previously authed client"}))
+                self.socket.sendTextMessage(json.dumps({"challenge_code_accepted": False, "error": "Challenge code did not match for previously authed client"}))
                 sender.close()
                 print("Client disconnected as challenge code did not match")
-           
-    def check_key(self,json_data):
+
+    def check_key(self, json_data):
         try:
             if json_data["challenge_code"]:
                 return True
@@ -141,14 +132,12 @@ class WebSocketServer(QWebSocketServer):
             print(e)
             return False
 
-
     def process_text_message(self, message):
         sender = self.sender()
         json_data = self.convert_str_to_json(message)
         sender_ip = sender.peerAddress().toString()
-        correct_challenge_code=self.check_key(json_data)
-        
-        
+        correct_challenge_code = self.check_key(json_data)
+
         if correct_challenge_code:
             received_challenge_code = json_data["challenge_code"]
             if received_challenge_code == self.challenge_code:
@@ -158,8 +147,8 @@ class WebSocketServer(QWebSocketServer):
                 self.text_edit.append("New Client authenticated")
                 self.text_edit.append("Client connected")
                 self.text_edit.append(f"Client address: {sender_ip}")
-                self.socket.sendTextMessage(json.dumps({"challenge_code":  self.challenge_code,
-                                                        "challenge_code_accepted":True}))
+                self.socket.sendTextMessage(json.dumps({"challenge_code": self.challenge_code,
+                                                         "challenge_code_accepted": True}))
                 self.save_authed_clients()
                 self.socket.textMessageReceived.connect(self.process_auth_text_message)
             else:
@@ -167,33 +156,48 @@ class WebSocketServer(QWebSocketServer):
                 self.text_edit.append("Challenge code did not match")
                 self.text_edit.append("Client not authenticated")
                 self.text_edit.append("Client disconnected")
-                self.socket.sendTextMessage(json.dumps({"challenge_code_accepted":False,"error":"Challenge code did not match for new client"}))
+                self.socket.sendTextMessage(json.dumps({"challenge_code_accepted": False, "error": "Challenge code did not match for new client"}))
                 sender.close()
         else:
             print("Challenge code not found in json data")
             self.text_edit.append("Client not authenticated")
             self.text_edit.append("Client disconnected")
-            self.socket.sendTextMessage(json.dumps({"challenge_code_accepted":False,"error":"Challenge code not found in json data"}))
+            self.socket.sendTextMessage(json.dumps({"challenge_code_accepted": False, "error": "Challenge code not found in json data"}))
             sender.close()
 
-    
     def client_disconnected(self):
         print("Client disconnected from server")
 
     def convert_str_to_json(self, data):
         try:
-            data=json.loads(data)
+            data = json.loads(data)
             return data
-    
         except Exception as e:
             print(e)
             return None
+
+    def on_pause(self):
+        self.text_edit.append("pause")
+        #  call manager to pause activity
+
+    def on_play(self):
+        self.text_edit.append("play")
+        #  call manager to play activity
+        
+    def on_pop(self):
+        self.text_edit.append("next")
+        # send next top activity to client
+
+    def on_new_activity(self):
+        self.text_edit.append("new activity received")
+        #  Update db with new activity 
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("WebSocket Server")
-        self.setGeometry(100, 100,700, 700)
+        self.setGeometry(100, 100, 700, 700)
         self.box_layout = QVBoxLayout()
         self.text_edit = QTextEdit(self)
         self.text_edit.setReadOnly(True)
@@ -228,15 +232,14 @@ class MainWindow(QMainWindow):
                 self.text_edit.append(f"Server is not listening on {self.locale_ip}:{WEB_SOCKET_SERVER_PORT}")
         else:
             self.text_edit.append("No internet connection")
-        # self.send_button.clicked.connect(self.send_message)
 
     def generate_challenge_code(self, length=15):
         '''Generates a random string of uppercase letters and digits
         '''
-        return ''.join(random.choices(string.ascii_uppercase + 
-                                    string.digits+
-                                    string.ascii_lowercase,
-                                     k=length))
+        return ''.join(random.choices(string.ascii_uppercase +
+                                      string.digits +
+                                      string.ascii_lowercase,
+                                      k=length))
 
     def generate_qr_code(self, data):
         '''Secure QR code generator
@@ -247,16 +250,15 @@ class MainWindow(QMainWindow):
         Returns:
             str: Base64 encoded QR code image
         '''
-       
-        qr=QRCode(json.dumps(data))
+
+        qr = QRCode(json.dumps(data))
         temp_qr_image_path = os.path.join(os.path.dirname(__file__), "temp_qr_image.png")
-        qr.png(temp_qr_image_path,scale=8)
+        qr.png(temp_qr_image_path, scale=8)
         with open(temp_qr_image_path, "rb") as f:
             base64_encoded = base64.b64encode(f.read())
         os.remove(temp_qr_image_path)
-        self.label.setPixmap
         return base64_encoded
-    
+
     def set_qr_code(self):
         '''Sets the QR code image to the label
 
@@ -264,27 +266,27 @@ class MainWindow(QMainWindow):
             data (str): Base64 encoded QR code image
         '''
         self.secret_gen_code = self.generate_challenge_code()
-        self.qr_data={
-            "Secret":self.secret_gen_code,
-            "IP":self.locale_ip,
-            "Port":WEB_SOCKET_SERVER_PORT
+        self.qr_data = {
+            "Secret": self.secret_gen_code,
+            "IP": self.locale_ip,
+            "Port": WEB_SOCKET_SERVER_PORT
         }
         print(self.qr_data)
         self.qr_code = self.generate_qr_code(self.qr_data)
-        pixmap=QPixmap()
+        pixmap = QPixmap()
         pixmap.loadFromData(base64.b64decode(self.qr_code))
         self.label.setPixmap(pixmap)
-        self.label.setGeometry(10, 200, 300,300)
+        self.label.setGeometry(10, 200, 300, 300)
         self.label.show()
         self.text_edit.setGeometry(10, 10, 480, 180)
-        self.__codes=[]
+        self.__codes = []
         self.__codes.append(self.secret_gen_code)
         self.server.update_challenge_code(self.__codes)
-        
-      
+
+
 if __name__ == "__main__":
-    WEB_SOCKET_SERVER_PORT =8888
+    WEB_SOCKET_SERVER_PORT = 8888
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    main_window.show()    
+    main_window.show()
     sys.exit(app.exec())
